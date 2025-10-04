b152a677280bfb72bee84bab6ead5bc5aa5abf03
Sumanshu <101553319+SumanshuBorkar@users.noreply.github.com>
2025-10-04 22:38:10 +0530

    check

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from selenium.webdriver.chrome.options import Options
 
 app = FastAPI(title="Job Scraper API", version="1.1")
 
-JOB_URL = "https://careers-aeieng.icims.com/jobs/5417/engineering-data-analyst/job"
+JOB_URL = "https://careers-aeieng.icims.com/jobs/5417/engineering-data-analyst/job?mobile=false&width=1920&height=500&bga=true&needsRedirect=false&jan1offset=330&jun1offset=330"
 
 @app.get("/")
 def home():
